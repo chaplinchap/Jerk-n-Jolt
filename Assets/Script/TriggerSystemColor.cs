cc9692325da35e53bf062cb02eebb3dd89f8fab7
LameIkon <troelsjelsbak@hotmail.com>
2023-10-26 13:54:17 +0200

    Here we go again

--- Assets/Script/TriggerSystemColor.cs
+++ Assets/Script/TriggerSystemColor.cs
@@ -3,35 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Trigger : MonoBehaviour
+public class TriggerSystemColor : MonoBehaviour
 {
-    public GameObject platform;
-    public GameObject trigger;
+    public GameObject[] platforms;
+    public GameObject triggerSwitch;
     private float timeBox;
     public float coolDown;
+    public Color platformColor;
+    public Color turnedOffColor;
 
-    public void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Pusher") || collision.CompareTag("Puller"))
         {
-            Debug.Log("Something disappeared...");
-            platform.GetComponent<BoxCollider2D>().enabled = false;
-            trigger.GetComponent<CircleCollider2D>().enabled = false;
-            platform.GetComponent<SpriteRenderer>().color = new Color(255, 255, 0, 0.02f);
-            trigger.GetComponent<SpriteRenderer>().color = new Color(255, 255, 0, 0.02f);
+            //Debug.Log("Something disappeared...");
+
+            Platforms(false, turnedOffColor);
             timeBox = Time.time;
         }
     }
 
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Time.time - timeBox > coolDown)
         {
-            platform.GetComponent<BoxCollider2D>().enabled = true;
-            trigger.GetComponent<CircleCollider2D>().enabled = true;
-            platform.GetComponent<SpriteRenderer>().color = new Color(255f, 255f, 0, 1f);
-            trigger.GetComponent<SpriteRenderer>().color = new Color(255f, 255, 0, 1f);
+            Platforms(true, platformColor);
         }
+    }
+
+
+    // METHODS //
+
+    /** Platforms takes all the platforms in the level that is assigned to it and changes there state
+     *  It takes in a bool that if true will set the platforms colliders to on
+     *  Aswell as a color which it should turn into
+     */
+    private void Platforms(bool turn, Color changeColor)
+    {
+
+
+        for (int i = 0; i < platforms.Length; i++)
+        {
+            platforms[i].GetComponent<BoxCollider2D>().enabled = turn;
+            platforms[i].GetComponent<SpriteRenderer>().color = changeColor;
+        }
+
+        triggerSwitch.GetComponent<CircleCollider2D>().enabled = turn;
+        triggerSwitch.GetComponent<SpriteRenderer>().enabled = turn;
+
     }
 }
