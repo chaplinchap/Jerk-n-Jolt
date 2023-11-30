2f63f8e2101937fc88c49cce44cc931d3cd43691
LameIkon <troelsjelsbak@hotmail.com>
2023-11-30 06:05:15 +0100

    Win Animations

--- Assets/Animations/PlayerAnimations/Puller/PullerAnimator.cs
+++ Assets/Animations/PlayerAnimations/Puller/PullerAnimator.cs
@@ -49,6 +49,7 @@ public class PullerAnimator : AnimationsParent
     {
         if (Time.time < lockStateTimer) return currentState;
 
+        //if (WhenGameIsOverBool!!!) return LockState(WinAnim(), 2f);
 
         if (isRespawing) return LockState(spawning, GetRespawnDuration() + 0.1f);
         if (stunScript.IsStunned()) return Stun();
